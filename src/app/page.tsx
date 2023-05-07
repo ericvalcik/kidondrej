572c472d175868671a324c02456fcad21c50ad98
eric valcik <eric.valcik@gmail.com>
2023-05-07 15:08:33 +0200

    Scroll working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { useEffect, useMemo, useRef } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 
 export default function Home() {
+  const [showScroll, setShowScroll] = useState(false)
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const html = document.documentElement
   let canvas = canvasRef.current
@@ -26,9 +27,6 @@ export default function Home() {
     if (canvas) {
       canvas.height = window.window.innerHeight
       canvas.width = 640
-      if (context) {
-        context.font = '50px monospace'
-      }
     }
   }, [canvasRef])
 
@@ -38,27 +36,19 @@ export default function Home() {
       if (context && img) context.drawImage(img, 0, 0)
     }
     const onScroll = () => {
+      if (showScroll) setShowScroll(false)
       const scrollTop = html.scrollTop
       const scrollBottom = html.scrollHeight - html.clientHeight - scrollTop
-      const scrollFraction = (scrollTop - window.innerHeight) / (window.innerHeight * 4)
+      const scrollFraction = scrollTop / (window.innerHeight * 4)
       const frameIndex = Math.max(0, Math.min(frameCount - 1, Math.ceil(scrollFraction * frameCount)))
       window.requestAnimationFrame(() => updateImage(frameIndex + 1))
       // is currently in view (overriding scroll behavior)
-      if (
-        canvas &&
-        scrollTop > window.innerHeight &&
-        scrollBottom > window.innerHeight &&
-        canvas?.style.position !== 'fixed'
-      ) {
+      if (canvas && scrollBottom > window.innerHeight && canvas?.style.position !== 'fixed') {
         canvas.style.top = '0'
         canvas.style.position = 'fixed'
       }
       // is going out of view (normal scroll behaviour)
-      if (
-        canvas &&
-        (scrollTop < window.innerHeight || scrollBottom < window.innerHeight) &&
-        canvas?.style.position !== 'absolute'
-      ) {
+      if (canvas && scrollBottom < window.innerHeight && canvas?.style.position !== 'absolute') {
         canvas.style.position = 'absolute'
         if (scrollBottom < window.innerHeight) {
           canvas.style.top = `${html.scrollHeight - window.innerHeight * 3}px`
@@ -70,30 +60,50 @@ export default function Home() {
   }, [canvas, context, html.clientHeight, html.scrollHeight, html.scrollTop, images])
 
   useEffect(() => {
-    const img = new Image()
-    img.src = currentFrame(1)
-    if (canvas && context) {
-      canvas.width = 640
-      canvas.height = 770
-      img.onload = function () {
-        if (context) context.drawImage(img, 0, 0)
+    if (!showScroll) {
+      const img = new Image()
+      if (canvas && context) {
+        canvas.width = 640
+        canvas.height = 770
+        canvas.style.position = 'fixed'
+        canvas.style.top = '0'
+        img.src = currentFrame(1)
+        img.onload = function () {
+          if (context) context.drawImage(img, 0, 0)
+        }
       }
     }
   }, [canvas, context])
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowScroll(true)
+    }, 3000)
+    return () => clearTimeout(timeout)
+  }, [])
+
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-[640px] flex flex-col items-center">
-        <div className="w-full h-screen flex items-center justify-center">
-          <div className="font-bold text-4xl tracking-tighter">kidOndrej</div>
-        </div>
-        <div className="w-full h-[500vh] relative overflow-hidden">
-          <canvas ref={canvasRef} />
-        </div>
-        <div className="w-full h-screen flex items-center justify-center bg-amber-300">
-          <div className="font-bold text-4xl tracking-tighter">end form</div>
+    <>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="w-[640px] flex flex-col items-center">
+          <div className="w-full h-screen flex items-center justify-center">
+            <div className="font-bold text-4xl tracking-tighter z-10 pb-[65vh]">kidOndrej</div>
+          </div>
+          <div className="w-full h-[400vh] relative overflow-hidden">
+            <canvas ref={canvasRef} />
+          </div>
+          <div className="w-full h-screen flex items-center justify-center bg-amber-300">
+            <div className="font-bold text-4xl tracking-tighter">end form</div>
+          </div>
         </div>
       </div>
-    </div>
+      <div
+        className={`fixed bottom-0 w-screen p-6 text-center transition-all duration-200 ${
+          showScroll ? 'opacity-100' : 'opacity-0'
+        }`}
+      >
+        Scroll
+      </div>
+    </>
   )
 }
