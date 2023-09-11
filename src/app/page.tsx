8de14f538c6194824e0cad880eb93c5256a17dcb
eric valcik <eric.valcik@gmail.com>
2023-09-11 17:05:00 +0200

    make unscrollable for mible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex flex-col items-center justify-center pt-[40vh]">
       <h1 className="font-bold tracking-tighter">SOON</h1>
     </main>
   )
