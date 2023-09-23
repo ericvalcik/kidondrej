f6910fad78d2edef654957e8783e7c3c154b4c60
eric valcik <eric.valcik@gmail.com>
2023-09-23 15:03:07 +0200

    add mobile responsivness

--- src/app/tabs/layout.tsx
+++ src/app/tabs/layout.tsx
@@ -10,18 +10,18 @@ export default function PagesLayout({
   return (
     <div className="flex flex-col">
       <nav className="w-full h-16 mb-20 backdrop-blur sticky top-0 shadow-sm">
-        <div className="max-w-4xl px-4 h-full mx-auto flex justify-between items-center">
+        <div className="max-w-5xl px-4 h-full mx-auto flex justify-between items-center">
           <Link
             href="/"
             className="font-bold tracking-tighter no-underline flex items-center gap-4"
           >
             <KidOndrejAvatar small />
-            <span className="text-xl">kidOndrej</span>
+            <span className="text-xl hidden sm:inline">kidOndrej</span>
           </Link>
           <Navbar />
         </div>
       </nav>
-      <div className="max-w-3xl w-full mx-auto">{children}</div>
+      <div className="max-w-2xl w-full mx-auto px-6">{children}</div>
     </div>
   );
 }
