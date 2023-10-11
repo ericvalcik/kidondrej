7e0c4ac3307a32d47afe1167c756f02835dc78f7
eric valcik <eric.valcik@gmail.com>
2023-10-11 16:37:33 +0200

    change bg color + start projects page

--- src/app/tabs/projects/page.tsx
+++ src/app/tabs/projects/page.tsx
@@ -1,31 +1,15 @@
 "use client";
 
 import React, { useState } from "react";
-import { Video } from "@/components/Video";
+import { P } from "@/components/typography";
 
 export default function Page() {
   const [playing, setPlaying] = useState("");
 
   return (
-    <div className="columns-1 gap-4 sm:columns-2">
-      <Video
-        videoUrl="/similivinlife/klip.mp4"
-        name="simi-klip"
-        setPlaying={setPlaying}
-        playing={playing}
-      />
-      <Video
-        videoUrl="/iann/nft-promo.mp4"
-        name="nft-promo"
-        setPlaying={setPlaying}
-        playing={playing}
-      />
-      <Video
-        videoUrl="/iann/visual.mp4"
-        name="iann-visual"
-        setPlaying={setPlaying}
-        playing={playing}
-      />
-    </div>
+    <>
+      <P>Here are some of my projects:</P>
+      <div className="flex"></div>
+    </>
   );
 }
